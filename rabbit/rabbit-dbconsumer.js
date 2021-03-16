86e532e569c55ae6ba93943269c175662ef83e6c
Georgia A. Kalitsi <43840728+georgiakal22@users.noreply.github.com>
2021-03-16 15:22:38 +0200

    Add files via upload

--- rabbit/rabbit-dbconsumer.js
+++ rabbit/rabbit-dbconsumer.js
@@ -1,8 +1,10 @@
-var amqlib = require('amqplib');
+var amqp = require('amqplib');
 const { fineStructureDependencies } = require('mathjs');
 var mongo = require('mongod');
 const { Collection } = require('mongoose');
 var _ = require('underscore');
+const MongoClient = require('mongodb').MongoClient;
+
 
 var TaskBroker = function(){
     this.queueName = 'web';
@@ -10,30 +12,47 @@ var TaskBroker = function(){
     this.mongo= {};
 };
 
+// TaskBroker.prototype.onConnect= function (connection){
+//   this.rabbit.connection = connection;
+//   return connection.createChannel();
+// };
+
+TaskBroker.prototype.onChannelCreated= function (channel) {
+  this.rabbit.channel = channel;
+  return channel.assertQueue(this.queueName, {durable: true});
+};
+
 TaskBroker.prototype.connectRabbit = function(){
     return amqp.connect('amqp://localhost:5672')
-
     .then(function onConnect(connection) {
-        this.rabbit.connection = connection;
-        return connection.createChannel()
-      }.bind(this))
-  
-      .then(function onChannelCreated(channel) {
-        this.rabbit.channel = channel;
-        return channel.assertQueue(this.queueName, {durable: true});
+      this.rabbit.connection = connection;
+      return connection.createChannel()
     }.bind(this))
+
+    .then(function onChannelCreated(channel) {
+      this.rabbit.channel = channel;
+      return channel.assertQueue(this.queueName, {durable: true});
+    }.bind(this))
+
 };
 
 TaskBroker.prototype.connectMongo = function(){
     return function(){
-        this.mongo.db = mongo('mongodb://127.0.0.1:27017/dockerApp',['Ιnput']);
+        const client = new MongoClient("mongodb://localhost:27017/dockerApp", {useNewUrlParser:true});
+        client.connect(err=>{
+          this.mongo.db = client.db("dockerApp").collection("User");
+          client.close();
+
+        });
+        
+                
         return this.mongo.db;
     }.bind(this);
 };
 
 TaskBroker.prototype.connect = function(){
     return this.connectRabbit()
-    .then(this.connectMongo());
+    .then(this.connectMongo())
 };
 
 TaskBroker.prototype.disconnect = function(){
@@ -45,8 +64,11 @@ TaskBroker.prototype.disconnect = function(){
 
 
 TaskBroker.prototype.getTask = function() {
-    this.rabbit.channel.sendToQueue(this.queueName,new Buffer(JSON.stringify(this.mongo.db.input.find({}))), {deliveryMode:true});
-    console.log("Successfully took db object");
+  this.mongo.db.find({}).toArray(function(err,result){
+    if(err) throw err;
+    this.rabbit.channel.sendToQueue(this.queueName,new Buffer(result), {deliveryMode:true});
+  });
+  console.log("Successfully took db object");
     
 };
 
